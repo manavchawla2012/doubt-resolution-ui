ad55420e0c571951ab9609b8005cd4f832b86937
manav chawla <manav@alphacare.xyz>
2021-06-28 01:50:34 +0530

    added return

--- src/redux/actions/commonAction.js
+++ src/redux/actions/commonAction.js
@@ -2,7 +2,7 @@ import {LOADER} from "../type/commonType";
 
 export const setLoader = (state=false) => {
     return dispatch => {
-        dispatch({
+        return dispatch({
             type: LOADER,
             loader: state
         })
